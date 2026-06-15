bb5fa63ec133b009f7b909e71b3ab27287de2d45
青衫依旧 <2461521018@qq.com>
2026-06-15 10:16:14 +0800

    chore: docs pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vitepress";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isGithubActions && repoName ? `/${repoName}/` : "/",
   lang: "zh-CN",
   title: "Desktop Infra Starter Docs",
   description: "Electron + Vue + NestJS 纯框架文档",
